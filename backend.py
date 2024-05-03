5014c3e597e6bfaef40e413e45099006f6021e24
Firas Zebib <firas.zebib@bain.com>
2024-05-03 12:54:43 -0400

    Complete backend functionality

--- backend.py
+++ backend.py
@@ -6,13 +6,21 @@ import requests
 WEATHER_APP_API_KEY = "WEATHER_APP_API_KEY"
 WEATHER_APP_API_KEY = os.getenv(WEATHER_APP_API_KEY)
 
-def get_data(place, forecast_Days=None, kind=None):
+def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={WEATHER_APP_API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    forecast_days = forecast_days
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=5, kind="Sky"))
 
 
